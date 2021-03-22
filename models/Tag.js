1397bf0cd76338b8967277d92f6cac8655a8c99a
FlipFlaptheTraitor <flipflapthetraitor@protonmail.com>
2021-03-22 00:11:28 -0400

    Readme setup

--- models/Tag.js
+++ models/Tag.js
@@ -5,8 +5,16 @@ const sequelize = require('../config/connection.js');
 class Tag extends Model {}
 
 Tag.init(
-  {
-    // define columns
+  {// define columns
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      allowNull: false,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING
+    }
   },
   {
     sequelize,
